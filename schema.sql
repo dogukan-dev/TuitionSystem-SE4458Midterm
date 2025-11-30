4a2e2c3257a49c32453d931c2dd5a43ee247765b
dogukan-dev <ddyyyydd1234@outlook.com>
2025-11-30 16:34:26 +0300

    schema ordering fixed for deployment

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS student (
+    student_no          VARCHAR(11) PRIMARY KEY,
+    balance             DOUBLE PRECISION NOT NULL,
+    daily_payment_limit INT NOT NULL DEFAULT 3,
+    CONSTRAINT daily_payment_limit_nonnegative CHECK (daily_payment_limit >= 0)
+);
+
 CREATE TABLE IF NOT EXISTS account (
     account_no                  SERIAL PRIMARY KEY,
     student_no                  VARCHAR(11) NOT NULL UNIQUE,
@@ -6,12 +13,6 @@ CREATE TABLE IF NOT EXISTS account (
     CONSTRAINT fk_student FOREIGN KEY (student_no) REFERENCES student(student_no)
 );
 
-CREATE TABLE IF NOT EXISTS student (
-    student_no          VARCHAR(11) PRIMARY KEY,
-    balance             DOUBLE PRECISION NOT NULL,
-    daily_payment_limit INT NOT NULL DEFAULT 3,
-    CONSTRAINT daily_payment_limit_nonnegative CHECK (daily_payment_limit >= 0)
-);
 
 CREATE TABLE IF NOT EXISTS tuition (
     tuition_id          SERIAL PRIMARY KEY,
